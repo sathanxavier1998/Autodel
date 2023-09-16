b34aca17a2c7f45a9c66768320bd8341af630efa
sathanxavier1998 <71935696+sathanxavier1998@users.noreply.github.com>
2023-09-17 01:05:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ from subprocess import Popen
 from pyrogram import Client, filters
 
 User = Client("auto-delete-user",
-               session_string=SESSION)
+                  session_string=SESSION)
 
 @User.on_message(filters.chat(CHATS))
 async def delete(user, message):
